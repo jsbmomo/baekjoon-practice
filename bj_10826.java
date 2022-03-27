975b4bbf14a9024230c8b51eba1727bc174a43d4
jsbmomo <tytyes40@gmail.com>
2022-03-27 13:56:49 +0900

    백준 10826 에러 수정 및 개선

--- bj_10826.java
+++ bj_10826.java
@@ -1,24 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
+
 
 public class bj_10826 {
 
-  private static int[] register;
+	private static BigInteger[] bi;
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int input = Integer.parseInt(br.readLine());
+		int input = Integer.parseInt(br.readLine());
 
-    register = new int[input + 1];
+		
+		if (input < 2) {
+			System.out.println(input);
+			return;
+		}
 
-    System.out.println(fibonacci(input));
+		bi = new BigInteger[input + 1];
+		bi[0] = BigInteger.ZERO;
+		bi[1] = BigInteger.ONE;
+
+		for (int idx = 2; idx <= input; idx++) {
+			bi[idx] = bi[idx - 2].add(bi[idx - 1]);
+		}
+    
+		System.out.println(bi[input]);
   }
   
-
-  public static int fibonacci(int num) {
-    if (num <= 1) return num;
-    else if (register[num] != 0) return register[num];
-    else return register[num] = fibonacci(num - 1) + fibonacci(num - 2);
-  }
 }

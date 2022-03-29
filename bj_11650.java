5b44e2a49468b950373de4d6921f7f3895de0cc9
jsbmomo <tytyes40@gmail.com>
2022-03-29 23:49:30 +0900

    백준 11727

---
+++ bj_11650.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class bj_11650 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int line = Integer.parseInt(br.readLine());
+    
+    for (int cnt = 0; cnt < line; cnt++) {
+      
+    }
+
+
+  }
+}

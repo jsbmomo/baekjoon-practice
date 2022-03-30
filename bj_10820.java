9a3d68e706a251de0f610ad99c95cc9db563cf0f
jsbmomo <tytyes40@gmail.com>
2022-03-31 00:40:33 +0900

    백준 10820 문자열 소문자, 대문자, 숫자, 공백 개수 카운트

--- bj_10820.java
+++ bj_10820.java
@@ -1,36 +1,41 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.IOException;
 
 public class bj_10820 {
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
     int line = Integer.parseInt(br.readLine());
-    
+    int[][] cnt = new int[line][4];
+
     for (int i = 0; i < line; i++) {
       String input = br.readLine();
-
+      
       for (int j = 0; j < input.length(); j++) {
-        int[] cnt = new int[4];
-
         if (input.charAt(j) >= 'a' && input.charAt(j) <= 'z') {
-          cnt[0]++;
+          cnt[i][0]++;
+        } else if (input.charAt(j) >= 'A' && input.charAt(j) <= 'Z') {
+          cnt[i][1]++;
+        } else if (input.charAt(j) >= '0' && input.charAt(j) <= '9') {
+          cnt[i][2]++;
+        } else if (input.charAt(j) == ' ') {
+          cnt[i][3]++;
         }
-
-        if (input.charAt(j) >= 'A' && input.charAt(j) <= 'Z') {
-          cnt[1]++;
-        }
-        
-        if (input.charAt(i) >= '0' && input.charAt(i) <= '9') {
-          cnt[2]++;
-        }
-        
-        if (input.charAt(i) == ' ') {
-          cnt[3]++;
-        }
-        
       }
     }
+
+    for (int w = 0; w < line; w++) {
+      for (int z = 0; z < cnt[w].length; z++) {
+        bw.write(cnt[w][z] + " ");
+      }
+      bw.write("\n");
+    }
+
+    bw.flush();
   }
 }

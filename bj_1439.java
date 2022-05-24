e2dc106a2f04f03c3508472c09daecf39fdfcdeb
jsbmomo <tytyes40@gmail.com>
2022-05-25 00:40:38 +0900

    백준 1439 이진수 뒤집기 여러방법

--- bj_1439.java
+++ bj_1439.java
@@ -10,26 +10,42 @@ public class bj_1439 {
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     String input = br.readLine();
 
-		int one = 0;
-		int zero = 0;
+    // case 1.
+		// int one = 0;
+		// int zero = 0;
+		// if(input.charAt(0) == '0') {
+    //   zero++;
+    // } else {
+    //   one++;
+    // }
+    // for(int i = 1; i < input.length(); i++) {
+    //   if (input.charAt(i - 1) != input.charAt(i)) {
+    //     if (input.charAt(i) == '0') {
+    //       zero++;
+    //     } else {
+    //       one++;
+    //     }
+    //   }
+		// }
 
-		if(input.charAt(0) == '0') {
-      zero++;
-    } else {
-      one++;
-    }
+    // case 2.
+    // int count = 0;
+		// for(int i = 1; i < input.length(); i++) {
+    //   if (input.charAt(i - 1) != input.charAt(i)) {
+    //     count++;
+    //   }
+		// }
+    // bw.append(((count + 1) / 2) + "\n");
 
+    // case 3.
+    int count = 0;
 		for(int i = 1; i < input.length(); i++) {
       if (input.charAt(i - 1) != input.charAt(i)) {
-        if (input.charAt(i) == '0') {
-          zero++;
-        } else {
-          one++;
-        }
+        count++;
       }
 		}
-	
-    bw.append((zero < one ? zero : one) + "\n");
+
+    bw.append(((count + 1) / 2) + "\n");
     bw.flush();
     bw.close();
     br.close();

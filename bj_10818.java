226922c9997baa0c216a4cd51bddb5a6a1ac3574
jsbmomo <tytyes40@gmail.com>
2022-05-26 21:56:07 +0900

    백준 10818, 최소 최대값 개선

--- bj_10818.java
+++ bj_10818.java
@@ -1,21 +1,55 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class bj_10818 {
-  
-  public static void main(String[] args) throws IOException {
-    Scanner scan = new Scanner(System.in);
-        
-    int N = scan.nextInt();
-    int[] arr = new int[N];
-        
-    for (int i = 0; i < N; i++) {
-      arr[i] = scan.nextInt();
+  static int[] values;
+  static StringTokenizer token;
+  static int max, min;
+  static int temp;
+
+  public static void main(String[] args) {
+    try (
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))
+    ) {
+      int count = Integer.parseInt(br.readLine());
+      values = new int[count];
+      token = new StringTokenizer(br.readLine());
+
+      // for (int index = 0; index < count; index++) {
+      //   values[index] = Integer.parseInt(token.nextToken());
+      // }
+
+      // max = min = values[0];
+      // for (int index = 0; index < count; index++) {
+      //   if (min > values[index]) {
+      //     min = values[index];
+      //   }
+      //   if (max < values[index]) {
+      //     max = values[index];
+      //   }
+      // }
+      max = min = Integer.parseInt(token.nextToken());
+      
+      while (token.hasMoreTokens()) {
+        temp = Integer.parseInt(token.nextToken());
+
+        if (min > temp) {
+          min = temp;
+        }
+        if (max < temp) {
+          max = temp;
+        }
+      }
+
+      bw.append(min + " " + max + "\n");
+      bw.flush();
+    } catch (IOException ignore) {
+      // java.io.* -> need to IOException
     }
-        
-    scan.close();
-    Arrays.sort(arr);
-    System.out.print(arr[0] + " " + arr[N - 1]);
   }
 }

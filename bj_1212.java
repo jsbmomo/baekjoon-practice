b324bea5044a32abfad9d2bd911342910c2ca259
jsbmomo <tytyes40@gmail.com>
2022-05-26 20:44:10 +0900

    백준 1212 8진수 2진수 변환 풀이

--- bj_1212.java
+++ bj_1212.java
@@ -5,39 +5,72 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class bj_1212 {
-
   static StringBuilder sb = new StringBuilder();
+  static String[] CASE = { "000", "001", "010", "011", "100", "101", "110", "111" };
 
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    char[] input = br.readLine().toCharArray();
+  public static void main(String[] args) {
+    try(
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))
+    ) {
+      String input = br.readLine();
 
-    convert(input[0], true);
-
-    for (int idx = 1; idx < input.length; idx++) {
-      convert(input[idx], false);
-    }
-
-    bw.append(sb.toString() + '\n');
-    bw.flush();
-    br.close();
-    bw.close();
-  }
-
-  public static void convert(char octal, boolean firstChar) {
-    int num = octal - 48;
-    int binary = 4; // 8진수를 2진수로 바꾸려면, 4 2 1 순으로 나눠야합니다.
-    for (int j = 2; j >= 0; j--) {
-      if ((num / binary) > 0 && num != 0) {
-        num -= binary;
-        sb.append('1');
-      } else {
-        if (!firstChar) {
-          sb.append('0');
-        } 
+      // for (int idx = 0; idx < input.length(); idx++) {
+      //   convert( input.charAt(idx) - '0' );
+      // }
+      
+      for (int index = 0; index < input.length(); index++) {
+        sb.append(CASE[input.charAt(index) - '0']);
       }
-      binary /= 2;
+
+      if (input.equals("0")) {
+        bw.append("0\n");
+        bw.flush();
+      } else {
+        while (sb.charAt(0) == '0') {
+          sb = new StringBuilder(sb.substring(1));
+        }
+        bw.append(sb.toString() + '\n');
+        bw.flush();
+      }
+    } catch (IOException ignore) {
+      // io 사용 시,  IOException
     }
   }
+
+  // public static void convert(int octal) {
+  //   switch(octal) {
+  //     case 0:
+  //       if (!frontJudge) {
+  //         sb.append(CASE[octal]);
+  //       }
+  //       break;
+  //     case 1:
+  //       if (frontJudge) {
+  //         sb.append('1');
+  //         frontJudge = false;
+  //       } else {
+  //         sb.append(CASE[octal]);
+  //       }
+  //       break;
+  //     case 2:
+  //       if (frontJudge) {
+  //         sb.append("10");
+  //         frontJudge = false;
+  //       } else {
+  //         sb.append(CASE[octal]);
+  //       }
+  //       break;
+  //     case 3:
+  //       if (frontJudge) {
+  //         sb.append("11");
+  //         frontJudge = false;
+  //       } else {
+  //         sb.append(CASE[octal]);
+  //       }
+  //       break;
+  //     default:
+  //       sb.append(CASE[octal]);
+  //   }
+  // }
 }

e3cdff8206b341b033d3fa35e16f52e6759f72f1
jsbmomo <tytyes40@gmail.com>
2022-06-23 23:51:56 +0900

    백준 10872 팩토리얼 풀기

---
+++ bj_10872.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 0보다 크거나 같은 정수 N이 주어진다. 이때, N!을 출력하는 프로그램을 작성하시오.
+ * 첫째 줄에 정수 N(0 ≤ N ≤ 12)이 주어진다.
+ */
+public class bj_10872 {
+  public static void main(String[] args) {
+    try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+      int input = Integer.parseInt(br.readLine());
+      System.out.println(factorial(input));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public static int factorial(int value) {
+    if (value == 0) {
+      return 1;
+    }
+    return value * factorial(--value);
+  }
+}

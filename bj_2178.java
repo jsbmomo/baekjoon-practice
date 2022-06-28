343c44cdd681e75fb178d0a035f2d37439d56174
jsbmomo <tytyes40@gmail.com>
2022-06-28 23:56:47 +0900

    백준 2178 미로 탐색(BFS) 풀이

---
+++ bj_2178.java
@@ -0,0 +1,87 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class bj_2178 {
+  /**
+    N×M크기의 배열로 표현되는 미로가 있다.
+    미로에서 1은 이동할 수 있는 칸을 나타내고, 0은 이동할 수 없는 칸을 나타낸다. 
+    이러한 미로가 주어졌을 때, (1, 1)에서 출발하여 (N, M)의 위치로 이동할 때 
+    지나야 하는 최소의 칸 수를 구하는 프로그램을 작성하시오. 
+    한 칸에서 다른 칸으로 이동할 때, 서로 인접한 칸으로만 이동할 수 있다.
+
+    ex)
+      1 0 1 1 1 1
+      1 0 1 0 1 0
+      1 0 1 0 1 1
+      1 1 1 0 1 1
+
+    위의 예에서는 15칸을 지나야 (N, M)의 위치로 이동할 수 있다. 
+    칸을 셀 때에는 시작 위치와 도착 위치도 포함한다.
+
+    입력 
+    첫째 줄에 두 정수 N, M(2 ≤ N, M ≤ 100)이 주어진다. 
+    다음 N개의 줄에는 M개의 정수로 미로가 주어진다. 
+    각각의 수들은 붙어서 입력으로 주어진다.
+   */
+  static int[][] map;
+  static int n;
+  static int m;
+  static boolean[][] visited;
+  static int[] dx = { -1, 1, 0, 0 };
+  static int[] dy = { 0, 0, -1, 1 };
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    n = Integer.parseInt(st.nextToken()); // 가로길이 입력
+    m = Integer.parseInt(st.nextToken()); // 세로길이 입력
+
+    map = new int[n][m];
+
+    // 101111 와 같이 입력되는 한 줄을 정수로 변환 후 배열에 하나씩 대입
+    for (int i = 0; i < n; i++) {
+      String s = br.readLine();
+      for (int j = 0; j< m; j++) {
+        map[i][j] = s.charAt(j) - '0';
+      }
+    }
+
+    visited = new boolean[n][m];
+    visited[0][0] = true; // 첫 시작 
+    bfs(0, 0);
+
+    System.out.println(map[n - 1][m - 1]);
+  }
+
+  public static void bfs(int x, int y) {
+    Queue<int[]> q = new LinkedList<>();
+    q.add(new int[] {x, y});
+
+    while (!q.isEmpty()) {
+      int now[] = q.poll();
+      int nowX = now[0];
+      int nowY = now[1];
+
+      for (int i = 0; i < 4; i++) {
+        int nextX = nowX + dx[i];
+        int nextY = nowY + dy[i];
+
+        if (nextX < 0 || nextY < 0 || nextX >= n || nextY >= m) {
+          continue;
+        }
+        if (visited[nextX][nextY] || map[nextX][nextY] == 0) {
+          continue;
+        }
+
+        q.add(new int[] {nextX, nextY});
+        map[nextX][nextY] = map[nowX][nowY] + 1;
+        visited[nextX][nextY] = true;
+      }
+    }
+  }
+  
+}

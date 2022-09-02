b368b91870d250f9645ee7094a32eac3af69fa34
jsbmomo <tytyes40@gmail.com>
2022-09-03 01:02:23 +0900

    백준 2908 상수, 다른 풀이

--- bj_2908.java
+++ bj_2908.java
@@ -1,22 +1,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.Inet4Address;
 
 public class bj_2908 {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int a = Integer.parseInt(br.readLine());
-    int b = Integer.parseInt(br.readLine());
+    String[] values = br.readLine().split(" ");
+    int[] reverseInt = new int[2];
+    
+    for (int index = 0; index < 2; index++) {
+      String reverse = "";
+      for (int i = values[index].length() - 1; i >= 0; i--) {
+        reverse += values[index].charAt(i);
+      }
+      reverseInt[index] = Integer.parseInt(reverse);
+    }
+    
+    System.out.println(reverseInt[0] > reverseInt[1] ? reverseInt[0] : reverseInt[1]);
 
-    StringBuilder sb = new StringBuilder();
+    /** Method 1. */
+    // StringTokenizer st = new StringTokenizer(br.readLine());
+    // StringBuilder sb = new StringBuilder(st.nextToken());
+    
+    // int a = Integer.parseInt(sb.reverse().toString());
 
-    a = Integer.parseInt(sb.append(a).reverse().toString());
+    // sb = new StringBuilder(st.nextToken());
+    // int b = Integer.parseInt(sb.reverse().toString());
 
-    sb = new StringBuilder();
+    /** Method 2. */
+    // StringBuilder sb = new StringBuilder();
+    // a = Integer.parseInt(sb.append(a).reverse().toString());
 
-    b = Integer.parseInt(sb.append(b).reverse().toString());
+    // sb = new StringBuilder();
+    // b = Integer.parseInt(sb.append(b).reverse().toString());
 
-    System.out.println(a > b ? a : b);
+    // System.out.println(a > b ? a : b);
   }
 }

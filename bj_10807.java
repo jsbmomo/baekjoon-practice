36aaa434af5cd230b405f29d16d3c30902151d52
jsbmomo <tytyes40@gmail.com>
2022-09-05 23:51:14 +0900

    백준 10807 개수 세기

---
+++ bj_10807.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bj_10807 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String temp = br.readLine();
+    int count = Integer.parseInt(temp.substring(0, temp.length() - 1));
+    int result = 0;
+
+    String[] values = br.readLine().split(" ");
+    temp = br.readLine();
+
+    String target = temp.substring(0);
+
+    for (String value : values) {
+      System.out.println(value + "  " + target);
+      if (value.equals(target)) {
+        result++;
+      }
+    }
+    
+    System.out.println(result);
+    br.close();
+  }
+}

86f204cf66c7d76b21f5ebcd66a4ca7cfde315c3
jsbmomo <tytyes40@gmail.com>
2022-09-25 00:05:18 +0900

    백준 2675 문자열 반복 최종

--- bj_2675.java
+++ bj_2675.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class bj_2675 {
   static BufferedReader br;
@@ -11,26 +11,23 @@ public class bj_2675 {
   static int repeatCount;
   static String repeatStr;
   static StringBuilder sb;
-  static String[] input;
+  static StringTokenizer st;
   static char[] strToChar;
 
   public static void main(String[] args) throws IOException {
     br = new BufferedReader(new InputStreamReader(System.in));
-    bw = new BufferedWriter(new OutputStreamWriter(System.out));
     count = Integer.parseInt(br.readLine());
     sb = new StringBuilder();
 
     for (int i = 0; i < count; i++) {
-      input = br.readLine().split(" ");
-      repeatCount = Integer.parseInt(input[0]);
-      repeatStr = input[1];
+      st = new StringTokenizer(br.readLine());
+      repeatCount = Integer.parseInt(st.nextToken());
+      repeatStr = st.nextToken();
 
       repeat();
     }
 
-    bw.append(sb.toString());
-    bw.flush();
-    bw.close();
+    System.out.println(sb.toString());
     br.close();
   }
 

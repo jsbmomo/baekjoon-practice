6b0095d1e380a268fdb994c85b4cdc05e68b8a41
jsbmomo <tytyes40@gmail.com>
2022-09-26 22:42:19 +0900

    백준 10250 ACM 호텔

---
+++ bj_10250.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj_10250 {
+  static BufferedReader br;
+  static StringTokenizer st;
+  static int count;
+  static int floor;
+  static int room;
+  public static void main(String[] args) throws IOException {
+    br = new BufferedReader(new InputStreamReader(System.in));
+    count = Integer.parseInt(br.readLine());
+
+    for (int i = 0; i < count; i++) {
+      st = new StringTokenizer(br.readLine());
+      
+      int H = Integer.parseInt(st.nextToken());
+      int W = Integer.parseInt(st.nextToken());
+      int N = Integer.parseInt(st.nextToken());
+
+      // 방법 2.
+      if (N % H == 0) {
+        floor = H * 100;
+        room = N / H;
+      } else {
+        floor = (N % H) * 100;
+        room = (N / H) + 1;
+      }
+
+      System.out.println((floor + room));
+      // 방법 1.
+      // for (int j = 1; j <= W; j++) {
+      //   for (int z = 1; z <= H; z++) {
+      //     if (++location == N) {
+      //       floor = z;
+      //       room = j;
+      //       break;
+      //     }
+      //   }
+      //   if (floor != 0) break;
+      // }
+
+      // System.out.println(String.format("%d%02d", floor, room));
+      // location = floor = room = 0;
+    }
+
+    br.close();
+  }
+}

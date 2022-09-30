e6c9c7d653379a3253741574890dd3a1533fb9c3
jsbmomo <tytyes40@gmail.com>
2022-10-01 00:18:15 +0900

    백준 10828 스택 구현, 방법 2

--- bj_10828.java
+++ bj_10828.java
@@ -7,45 +7,40 @@ import java.util.StringTokenizer;
 
 public class bj_10828 {
 
-  static int[] stack = new int[10001];
+  static int[] stack;
   static int index;
 
   static BufferedReader br;
   static BufferedWriter bw;
   static StringTokenizer st;
-  static StringBuilder sb;
   static int loop;
 
   public static void main(String[] args) throws IOException {
     br = new BufferedReader(new InputStreamReader(System.in));
     bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    sb = new StringBuilder();
     loop = Integer.parseInt(br.readLine());
+    stack = new int[loop + 1];
 
     for (int i = 0; i < loop; i++) {
       st = new StringTokenizer(br.readLine(), " ");
       String command = st.nextToken();
 
-      switch (command) {
-        case "push":
-          push(Integer.parseInt(st.nextToken()));
-          break;
-        case "pop":
-          sb.append(pop()).append('\n');
-          break;
-        case "size":
-          sb.append(size()).append('\n');
-          break;
-        case "empty":
-          sb.append(empty()).append('\n');
-          break;
-        case "top":
-          sb.append(top()).append('\n');
-          break;
+      if (command.equals("push")) {
+        push(Integer.parseInt(st.nextToken()));
+      } else if (command.equals("pop")) {
+        bw.write(pop());
+        bw.newLine();
+      } else if (command.equals("size")) {
+        bw.write(size());
+        bw.newLine();
+      } else if (command.equals("empty")) {
+        bw.write(empty());
+      } else {
+        bw.write(top());
+        bw.newLine();
       }
     }
 
-    bw.write(sb.toString());
     bw.flush();
     bw.close();
     br.close();
@@ -55,19 +50,19 @@ public class bj_10828 {
     stack[++index] = input;
   }
 
-  static int pop() {
-    return index != 0 ? stack[index--] : -1;
+  static String pop() {
+    return index != 0 ? String.valueOf(stack[index--]) : "-1";
   }
 
-  static int size() {
-    return index;
+  static String size() {
+    return String.valueOf(index);
   }
 
-  static int empty() {
-    return index == 0 ? 1 : 0;
+  static String empty() {
+    return index == 0 ? "1\n" : "0\n";
   }
 
-  static int top() {
-    return index != 0 ? stack[index] : -1;
+  static String top() {
+    return index != 0 ? String.valueOf(stack[index]) : "-1";
   }
 }

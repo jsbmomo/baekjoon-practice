415fa51d04392184881f1e7cb59c8cd67044cb8b
jsbmomo <tytyes40@gmail.com>
2022-10-13 23:14:20 +0900

    백준 2491 수정

--- bj_2491.java
+++ bj_2491.java
@@ -4,54 +4,49 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class bj_2491 {
+  static BufferedReader br;
+  static StringTokenizer st;
+  static int number;
+  static int[] ary;
+
   public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    br = new BufferedReader(new InputStreamReader(System.in));
     int number = Integer.parseInt(br.readLine());
-    int up = 0;
-    int upMax = 0;
-    int down = 0;
-    int downMax = 0;
-    int defore = 0;
-    boolean isUp = false;
+    ary = new int[number];
+    st = new StringTokenizer(br.readLine());
 
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    
-    while(st.hasMoreTokens()) {
-      int temp = Integer.parseInt(st.nextToken());
-
-      if (defore < temp) {
-        if (downMax < down && down != 0) {
-          downMax = down;
-        }
-        down = 0;
-        up++;
-        isUp = true;
-      } else if (defore > temp) {
-        if (upMax < up && up != 0) {
-          upMax = up;
-        }
-        up = 0;
-        down++;
-        isUp = false;
-      } else if (defore == temp) {
-        if (isUp) {
-          up++;
-        } else {
-          down++;
-        }
-      }
-
-      if (upMax < up) {
-        upMax = up;
-      }
-      if (downMax < down) {
-        downMax = down;
-      }
-      
-      System.out.println(downMax + "  " + upMax);
-      defore = temp;
+    for (int i = 0; i < number; i++) {
+      ary[i] = Integer.parseInt(st.nextToken());
     }
 
-    System.out.println(Math.max(upMax, downMax));
+    if (ary.length == 1){
+      System.out.println("1");
+      return ;
+    }
+
+    int big = 1;
+    int bigMax = 0;
+    for (int i = 0; i < number - 1; i++) {
+      if (ary[i] <= ary[i + 1]) big++;
+      else {
+        if (bigMax < big) bigMax = big;
+        big = 1;
+      }
+    }
+    if (bigMax < big) bigMax = big;
+
+    
+    int small = 1;
+    int smallMax = 0;
+    for (int i = 0; i < number - 1; i++) {
+      if (ary[i] >= ary[i + 1]) small++;
+      else {
+        if (smallMax < small) smallMax = small;
+        small = 1;
+      }
+    }
+    if (smallMax < small) smallMax = small;
+
+    System.out.println(Math.max(bigMax, smallMax));
   }
 }

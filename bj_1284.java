c3174ccf5a60013999cc9a9d08ff4c603a1d65f7
jsbmomo <tytyes40@gmail.com>
2022-10-28 23:59:08 +0900

    백준 1284 개선

--- bj_1284.java
+++ bj_1284.java
@@ -10,19 +10,19 @@ public class bj_1284 {
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     while(true) {
-      char[] input = br.readLine().toCharArray();
+      String input = br.readLine();
       int count = 0;
 
-      if (input[0] == '0') break;
+      if (input.equals("0")) break;
 
       count++;
-      for (int i = 0; i < input.length; i++) {
-        if (input[i] == '1') count += 3;
-        else if (input[i] == '0') count += 5;
+      for (int i = 0; i < input.length(); i++) {
+        if (input.charAt(i) == '1') count += 3;
+        else if (input.charAt(i) == '0') count += 5;
         else count += 4;
       }
 
-      bw.append(count + "\n");
+      bw.append(String.valueOf(count)).append("\n");
     }
 
     bw.flush();

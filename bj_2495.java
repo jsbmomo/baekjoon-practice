410cd00801458e47d8b4fd0cf3603bddb422fc3c
jsbmomo <tytyes40@gmail.com>
2022-11-15 00:22:36 +0900

    백준 2495 수정

--- bj_2495.java
+++ bj_2495.java
@@ -8,30 +8,31 @@ public class bj_2495 {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-    int count = 1;
-    int max = 0;
-    char temp = '0';
+    StringBuilder sb = new StringBuilder();
 
     for (int i = 0; i < 3; i++) {
       char[] numAry = br.readLine().toCharArray();
-      temp = numAry[0];
-      for (int j = 1; j < numAry.length; j++) {
+      char temp = numAry[0];
+      int count = 1;
+      int max = 0;
+      int aryLastIndex = numAry.length - 1; // 예제 마지막 값에 ' '이 포함됨
+      int len = numAry[aryLastIndex] == ' ' ? aryLastIndex - 1 : aryLastIndex;
+
+      for (int j = 1; j <= len; j++) {
         if (temp == numAry[j]) {
           count++;
         } else {
           temp = numAry[j];
-          max = count > max ? count : max;
+          max = count >= max ? count : max;
           count = 1;
         }
       }
 
-      bw.append(max + "\n");
-      temp = '0';
-      count = 1;
-      max = 0;
+      // 만약, 8자리 정수가 모두 같은 숫자라면 else를 거치지 않아, max 값은 비게된다.
+      sb.append(count >= max ? count : max).append('\n');
     }
     
+    bw.append(sb.toString());
     bw.flush();
     bw.close();
     br.close();

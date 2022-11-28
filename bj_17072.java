74140f9da7f1af14432854511d49c9918cf7ded1
jsbmomo <tytyes40@gmail.com>
2022-11-28 23:27:50 +0900

    백준 17072 아스키 아트

---
+++ bj_17072.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class bj_17072 {
+  static BufferedReader br = null;
+  static BufferedWriter bw = null;
+  static StringBuilder sb = null;
+
+  public static void main(String[] args) throws IOException {
+    br = new BufferedReader(new InputStreamReader(System.in));
+    bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    sb = new StringBuilder();
+
+    String[] range = br.readLine().split(" ");
+
+    for (int i = 0; i < Integer.parseInt(range[0]); i++) {
+      String[] line = br.readLine().split(" ");
+
+      for (int j = 0; j < line.length; j += 3) {
+        int r = Integer.parseInt(line[j]);
+        int g = Integer.parseInt(line[j + 1]);
+        int b = Integer.parseInt(line[j + 2]);
+        int multiply = intensityFunction(r, g, b);
+
+        sb.append(findAsciiCode(multiply));
+      }
+      sb.append('\n');
+    }
+
+    bw.append(sb.toString());
+    bw.flush();
+    bw.close();
+    br.close();
+  }
+
+  public static int intensityFunction(int r, int g, int b) {
+    return (2126 * r) + (7152 * g) + (722 * b);
+  }
+
+  public static char findAsciiCode(int value) {
+    if (isBetween(value, 0, 510_000)) return '#';
+    else if (isBetween(value, 510_000, 1_020_000)) return 'o';
+    else if (isBetween(value, 1_020_000, 1_530_000)) return '+';
+    else if (isBetween(value, 1_530_000, 2_040_000)) return '-';
+    else return '.';
+  }
+
+  public static boolean isBetween(int value, int lower, int upper) {
+    return lower <= value && value < upper;
+  }
+}

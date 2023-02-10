5673f2a520993903c1b6d6ef47b9a798ccfea7fe
jsbmomo <tytyes40@gmail.com>
2023-02-10 23:57:34 +0900

    백준 2563 색종이

---
+++ bj_2563.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj_2563 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int total = 0;
+    int count = Integer.parseInt(br.readLine());
+
+    boolean[][] ary = new boolean[101][101];
+
+    for (int i = 0; i < count; i++) {
+      StringTokenizer st =  new StringTokenizer(br.readLine());
+      int x = Integer.parseInt(st.nextToken());
+      int y = Integer.parseInt(st.nextToken());
+
+      for (int j = x; j < x + 10; j++) {
+        for (int k = y; k < y + 10; k++) {
+          if (!ary[j][k]) {
+            ary[j][k] = true;
+            total
+            ++;
+          }
+        }
+      }
+    }
+    
+    System.out.println(total);
+    br.close();
+  }
+}

173c317f760bc2230fb7b773756c3d76bed14bb0
jsbmomo <tytyes40@gmail.com>
2023-04-11 00:44:15 +0900

    백준 13223 개선

--- bj_13223.java
+++ bj_13223.java
@@ -3,33 +3,37 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class bj_13223 {
-  public static void main(String[] args) throws IOException{
+  public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    String[] startTime = br.readLine().split(":");
+    String[] currentTime = br.readLine().split(":");
     String[] endTime = br.readLine().split(":");
 
-    int[] needTime = new int[3];
+    int currentHour = Integer.parseInt(currentTime[0]);
+    int currentMinute = Integer.parseInt(currentTime[1]);
+    int currentSecond = Integer.parseInt(currentTime[2]);
+    int currentSecondAmount = currentHour * 3600 + currentMinute * 60 + currentSecond;
     
-    for (int i = 2; i >= 0; i--) {
-      needTime[i] = Integer.parseInt(endTime[i]) - Integer.parseInt(startTime[i]);
-    }
+    int endHour = Integer.parseInt(endTime[0]);
+    int endMinute = Integer.parseInt(endTime[1]);
+    int endSecond = Integer.parseInt(endTime[2]);
+    int endSecondAmount = endHour * 3600 + endMinute * 60 + endSecond;
 
-    if (needTime[2] < 0) {
-      needTime[1]--;
-      needTime[2] += 60;
-    }
+    int needSecondAmount = endSecondAmount - currentSecondAmount;
 
-    if (needTime[1] < 0) {
-      needTime[0]--;
-      needTime[1] += 60;
+    /**
+     * 문제의 조건을 보면 1초보다 크고 24시간보다 작은 값이 나와야한다.
+     * 고로, 00:00:00 은 나올 수 없다.
+     */
+    if (needSecondAmount <= 0) {
+      needSecondAmount += 24 * 3600;
     }
+    
+    int needHour = needSecondAmount / 3600;
+    int needMinute = (needSecondAmount % 3600) / 60;
+    int needSecond = needSecondAmount % 60;
 
-    if (needTime[0] < 0) {
-      needTime[0] += 24;
-    }
-
-    System.out.println(String.format("%02d:%02d:%02d", needTime[0], needTime[1], needTime[2]));
+    System.out.printf("%02d:%02d:%02d", needHour, needMinute, needSecond);
     br.close();
   }
 }

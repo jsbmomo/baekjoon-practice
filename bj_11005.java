81e35b2a27b95db86a90c8dacf0019d260fc3660
jsbmomo <tytyes40@gmail.com>
2023-04-16 22:55:33 +0900

    백준 11005 다시 풀어보기

--- bj_11005.java
+++ bj_11005.java
@@ -1,29 +1,30 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-
+import java.util.StringTokenizer;
 
 public class bj_11005 {
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringBuilder str = new StringBuilder();
-    String[] strs = br.readLine().split(" ");
-    int value = Integer.parseInt(strs[0]);
-    int notation = Integer.parseInt(strs[1]);
 
-    while (value > 0) {
-      int remain = value % notation;
-      
-      if (remain >= 10) {
-        str.append((char) (remain - 10 + 'A'));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int decimal = Integer.parseInt(st.nextToken());
+    int arithmetic = Integer.parseInt(st.nextToken());
+
+    StringBuilder sb = new StringBuilder();
+
+    while (decimal > 0) {
+      int temp = decimal % arithmetic;
+
+      if (temp < 10) {
+        sb.append(temp);
       } else {
-        str.append((char) (remain + '0'));    
+        sb.append((char) ((temp - 10) + 'A'));
       }
-      
-      value /= notation;
+
+      decimal /= arithmetic;
     }
-    
-    for (int i = str.length() - 1; i >= 0; i--) {
-      System.out.print(str.charAt(i));
-    }  
+
+    System.out.println(sb.reverse());
+    br.close();
   }
 }

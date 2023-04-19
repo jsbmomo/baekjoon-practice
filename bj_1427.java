1e68ecf18c4d44f4c2d6276b94ca2b791774737f
jsbmomo <tytyes40@gmail.com>
2023-04-19 23:58:23 +0900

    백준 1427 퀵정렬 적용

--- bj_1427.java
+++ bj_1427.java
@@ -6,16 +6,62 @@ import java.util.Arrays;
 public class bj_1427 {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String num = br.readLine();
+    String input = br.readLine();
 
-    char[] str = num.toCharArray();
+    int[] values = new int[input.length];
 
-    Arrays.sort(str);
-
-    for (int i = str.length - 1; i >= 0; i--) {
-      System.out.print(str[i]);
+    for (int i = 0; i < input.length; i++) {
+      values[i] = Integer.parseInt(input.charAt(i));
     }
 
+    pivotSort(value, 0, values.length);
+
+    StringBuilder sb = new StringBuilder();
+
+    for (int value : values) {
+      sb.append(value);
+    }
+
+    sb.append('\n');
+
+    System.out.println(sb.toString());
     br.close();
+  }
+
+  static void pivotSort(int[] ary, int low, int high) {
+    if (low > high) return;
+
+    int pivot = partition;
+
+    pivotSort(ary, low, pivot - 1);
+    pivotSort(ary, pivot + 1, high);
+  }
+
+  static int partition(int[] ary, int left, int right) {
+    int low = left;
+    int high = right;
+    int pivot = ary[right];
+
+    while (low < high) {
+      while(ary[low] < pivot && low < high) {
+        low++;
+      }
+
+      while(ary[high] >= pivot && low < high) {
+        high--;
+      }
+
+      swap(ary, low, high);
+    }
+
+    swap(ary, right, high);
+
+    return high;
+  }
+
+  static void swap(int[] a, int i, int j) {
+    int temp = a[i];
+    a[i] = a[j];
+    a[j] = temp;
   }
 }

ecedb84c44de508104ea72f6de010c479556e455
jsbmomo <tytyes40@gmail.com>
2023-04-28 00:00:35 +0900

    백준 10989 수정

--- bj_10989.java
+++ bj_10989.java
@@ -8,21 +8,18 @@ public class bj_10989 {
     int count = Integer.parseInt(br.readLine());
 
     int[] ary = new int[10001];
-    int[] storeIndex = new int[count + 1];
 
     for (int i = 1; i <= count; i++) {
       int input = Integer.parseInt(br.readLine());
       ary[input]++;
-      storeIndex[i] = input;
     }
 
     StringBuilder sb = new StringBuilder();
 
     for (int i = 1; i < 10001; i++) {
-      if (ary[i] > 0) {
-        for (int j = ary[i]; j > 0; j--) {
-          sb.append(i).append('\n');
-        }
+      while (ary[i] > 0) {
+        sb.append(i).append('\n');
+        i--;
       }
     }
 

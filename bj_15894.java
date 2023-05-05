b6f803f290b568bed4a459e28adcb3969ec8f9ad
jsbmomo <tytyes40@gmail.com>
2023-05-05 23:49:00 +0900

    백준 15894

---
+++ bj_15894.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bj_15894 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    long n = Long.parseLong(br.readLine());
+
+    System.out.println(4 * n);
+
+    br.close();
+  }
+}

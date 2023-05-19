0cb32171ede714aff7fda97105777f7407a47c1a
jsbmomo <tytyes40@gmail.com>
2023-05-19 23:22:37 +0900

    백준 10973 풀이

--- bj_10973.java
+++ bj_10973.java
@@ -21,6 +21,36 @@ public class bj_10973 {
     System.out.println(sb.toString());
   }
 
+  static boolean permutaion(int[] ary) {
+    int i = ary.length - 1;
+
+    while (i > 0 && ary[i] < ary[i - 1]) {
+      i--;
+    }
+
+    if (i == 0) {
+      return false;
+    }
+
+    int j = ary.length - 1;
+
+    while (ary[j] <= ary[i - 1]) {
+      j--;
+    }
+
+    swap(ary, i - 1, j);
+
+    j = ary.length - 1;
+
+    while (i < j) {
+      swap(ary, i, j);
+      i++;
+      j--;
+    }
+
+    return true;
+  }
+
   static void swap(int[] ary, int i, int j) {
     int temp = ary[i];
     ary[i] = ary[j];

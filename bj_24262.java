b650d45f90cc8209b43de33d7eae0af3b6e23f3d
jsbmomo <tytyes40@gmail.com>
2023-05-31 23:52:37 +0900

    백준 24262

---
+++ bj_24262.java
@@ -0,0 +1,9 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bj_24262 {
+  public static void main(String[] args) throws IOException {
+    System.out.println("1\n0");
+  }
+}

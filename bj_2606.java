11e57fbd5990ce0b72f471ab02a6520deaf71437
jsbmomo <tytyes40@gmail.com>
2023-12-31 23:45:33 +0900

    백준 2606 정답 오류 수정

--- bj_2606.java
+++ bj_2606.java
@@ -8,7 +8,7 @@ public class bj_2606 {
   static int result = 0;
   static int[][] pc;
   static boolean[] infected;
-  static final int virus = 1;
+  static final int VIRUS = 1;
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,16 +16,18 @@ public class bj_2606 {
     pcCount = Integer.parseInt(br.readLine());
     connected = Integer.parseInt(br.readLine());
 
-    pc = new int[pcCount + 1][2];
+    pc = new int[pcCount + 1][pcCount + 1];
     infected = new boolean[pcCount + 1];
 
     for (int i = 1; i <= connected; i++) {
       String[] str = br.readLine().split(" ");
-      pc[i][0] = Integer.parseInt(str[0]);
-      pc[i][1] = Integer.parseInt(str[1]);
+      int y = Integer.parseInt(str[0]);
+      int x = Integer.parseInt(str[1]);
+
+      pc[y][x] = pc[x][y] = 1;
     }
 
-    dfs(virus);
+    dfs(VIRUS);
 
     System.out.println(result);
     br.close();
@@ -33,10 +35,12 @@ public class bj_2606 {
 
   static void dfs(int pcName) {
     infected[pcName] = true;
-    result++;
 
     for (int i = 1; i <= pcCount; i++) {
-      if (pc[pcName][0] == 1 && !infected[i]) dfs(i);
+      if (pc[pcName][i] == 1 && !infected[i]) {
+        dfs(i);
+        result++;
+      }
     }
   }
 }

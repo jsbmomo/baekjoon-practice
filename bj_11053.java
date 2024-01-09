5c3fdafcf9db05865e8d804a8cc5b5f727724387
jsbmomo <tytyes40@gmail.com>
2024-01-09 23:57:07 +0900

    백준 11053 가장 긴 증가하는 부분 수열

---
+++ bj_11053.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class bj_11053 {
+
+  static int len, result;
+  static int[] A;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    len = Integer.parseInt(br.readLine());
+
+    if (len == 0) return;
+    
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    A = new int[len];
+
+    for (int i = len - 1; i >= 0; i--) {
+      A[i] = Integer.parseInt(st.nextToken());
+    }
+    
+    sort();
+
+    result++;
+    
+    for (int i = 1; i < len; i++) {
+      if (A[i - 1] != A[i] && A[i - 1] < A[i]) result++;
+    }
+
+    System.out.println(result);
+    br.close();
+  }
+
+  static void sort() {
+    int temp = 0;
+
+    for (int i = 0; i < len - 1; i++) {
+      for (int j = i + 1; j < len; j++) {
+        if (A[i] > A[j]) {
+          temp = A[i];
+          A[i] = A[j];
+          A[j] = temp;
+        }
+      }
+    }
+  }
+
+}

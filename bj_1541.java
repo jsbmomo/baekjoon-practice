c31f456848edb41893b6f44cb2601e122cd01763
jsbmomo <tytyes40@gmail.com>
2024-02-08 23:58:31 +0900

    백준 1541 잃어버린 괄호 - 입력 속도 개선

--- bj_1541.java
+++ bj_1541.java
@@ -1,24 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class bj_1541 {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    String[] inputs = br.readLine().split("-");
-    int len = inputs.length;
+    StringTokenizer inputs = new StringTokenizer(br.readLine(), "-");
+    StringTokenizer addition;
+
     int sum = Integer.MAX_VALUE;
 
-    for (int i = 0; i < len; i++) {
+    while(inputs.hasMoreTokens()) {
       // split의 경우 정규식을 받기에, 그냥 '+' 사용 시 PatternSyntaxException 발생
       // '+'는 메타 문자기에 그대로 사용하기 위해서는 이스케이프 처리를 해주어야 한다.
-      String[] addition = inputs[i].split("\\+");
+      // String[] addition = inputs[i].split("\\+");
+
+      addition = new StringTokenizer(inputs.nextToken(), "+");
 
       int temp = 0;
 
-      for (int j = 0; j < addition.length; j++) {
-        temp += Integer.parseInt(addition[j]);
+      while (addition.hasMoreTokens()) {
+        temp += Integer.parseInt(addition.nextToken());
       }
 
       if (sum == Integer.MAX_VALUE) sum = temp;
